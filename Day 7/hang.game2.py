1479c41982ef59e8e03df4c16aa1c06deca4783f
KajoDeza <diellzakajoshaj@gmail.com>
2025-02-24 19:42:10 +0100

    .

--- Day 7/hang.game2.py
+++ Day 7/hang.game2.py
@@ -5,11 +5,21 @@ word_list = ["Moon", "Life", "Psithurism", "Anaconda"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
+word_length = len(chosen_word)
+
+placeholder = ""
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
 the_guess = input("Guess a letter: ").lower()
 print(the_guess)
 
+display = ""
+
 for letter in chosen_word:
     if letter == the_guess:
-        print("Right")
+        display += letter
     else:
-        print("Wrong")
+        display += "_"
+print(display)
