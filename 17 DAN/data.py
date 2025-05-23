ef95fe82222a33bdc10ad0ff65f275d9a2b74c69
KajoDeza <diellzakajoshaj@gmail.com>
2025-05-23 16:02:58 +0200

    .

---
+++ 17 DAN/data.py
@@ -0,0 +1,95 @@
+question_data = [
+    {
+        "response_code": 0,
+        "results": [
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science &amp; Nature",
+                "question": "Shrimp can swim backwards.",
+                "correct_answer": "True",
+                "incorrect_answers": ["False"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science &amp; Nature",
+                "question": "The Neanderthals were a direct ancestor of modern humans.",
+                "correct_answer": "False",
+                "incorrect_answers": ["True"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science &amp; Nature",
+                "question": "Sound can travel through a vacuum.",
+                "correct_answer": "False",
+                "incorrect_answers": ["True"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science &amp; Nature",
+                "question": "Sugar contains fat.",
+                "correct_answer": "False",
+                "incorrect_answers": ["True"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science &amp; Nature",
+                "question": "Pneumonoultramicroscopicsilicovolcanoconiosis is a synonym for the disease known as silicosis.",
+                "correct_answer": "True",
+                "incorrect_answers": ["False"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science &amp; Nature",
+                "question": "Enthalpy is a measure of the energy that is not available for work during a thermodynamic process.",
+                "correct_answer": "False",
+                "incorrect_answers": ["True"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science &amp; Nature",
+                "question": "The &quot;Gympie Stinger&quot; is the deadliest plant in the world.",
+                "correct_answer": "False",
+                "incorrect_answers": ["True"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science &amp; Nature",
+                "question": "In the periodic table, Potassium&#039;s symbol is the letter K.",
+                "correct_answer": "True",
+                "incorrect_answers": ["False"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science &amp; Nature",
+                "question": "Type 1 diabetes is a result of the liver working improperly.",
+                "correct_answer": "False",
+                "incorrect_answers": ["True"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science &amp; Nature",
+                "question": "El Ni&ntilde;o pushes warm water to the coasts of the Western Pacific during winter.",
+                "correct_answer": "True",
+                "incorrect_answers": ["False"]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "medium",
+                "category": "Science &amp; Nature",
+                "question": "&quot;Tachycardia&quot; or &quot;Tachyarrhythmia&quot; refers to a resting heart-rate near or over 100 BPM.",
+                "correct_answer": "True",
+                "incorrect_answers": ["False"]
+            }
+        ]
+    }
+]
